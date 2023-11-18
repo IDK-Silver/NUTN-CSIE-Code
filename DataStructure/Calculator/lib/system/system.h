284a64634b760fe7af0d06ff1032c1db33d74296
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2023-11-18 18:20:40 +0800

    Update system.h

--- DataStructure/Calculator/lib/system/system.h
+++ DataStructure/Calculator/lib/system/system.h
@@ -9,9 +9,17 @@
 #include <stdio.h>
 
 void exit_with_press_any_key(int __status) {
+
+    fflush(stderr);
+
     printf("Enter any to exit.");
     char exit_char = 0;
+
     scanf("%c", &exit_char);
+    while (exit_char != '\n') {
+        scanf("%c", &exit_char);
+    };
+
     exit(__status);
 }
 
