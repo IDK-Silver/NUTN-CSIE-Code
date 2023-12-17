06f787f288212d08b86e7d2d96d7c47f740c4a8e
IDK-Silver <60951976+IDK-Silver@users.noreply.github.com>
2023-12-17 22:50:37 +0800

    complete binary search tree lib

--- DataStructure/BinarySearchTree/lib/bst/bst.h
+++ DataStructure/BinarySearchTree/lib/bst/bst.h
@@ -61,5 +61,135 @@ struct bst_head {
 } while(0)
 
 
+/**
+ * bst search - search ptr in tree that belong to root
+ * @ptr: want to find target
+ * @root: tree root
+ * @compare_func: compare entry function
+ */
+struct bst_head* bst_search(struct bst_head *ptr, struct bst_head *root,
+                            int (*compare_func)(struct bst_head*, struct bst_head*))
+{
+    while (root != NULL && compare_func(ptr, root) != 0)
+    {
+        if (compare_func(ptr, root) == -1)
+            root = root->left;
+        else
+            root = root->right;
+    }
+    return root;
+}
+
+/**
+ * bst minimum - find the minimum entry in tree that belong to ptr
+ * @ptr: the root
+ * @return return minimum entry in ptr
+ */
+struct bst_head* bst_minimum(struct bst_head *ptr)
+{
+    while (ptr->left != NULL)
+        ptr = ptr->left;
+    return  ptr;
+}
+
+/**
+ * bst bst_maximum - find the bst_maximum entry in tree that belong to ptr
+ * @ptr: the root
+ * @return return bst_maximum entry in ptr
+ */
+struct bst_head* bst_maximum(struct bst_head *ptr)
+{
+    while (ptr->right != NULL)
+        ptr = ptr->right;
+    return  ptr;
+}
+
+/**
+ * bst successor - find the entry with the smallest key greater than ptr key
+ * @ptr: the current node.
+ */
+struct bst_head* bst_successor(struct bst_head *ptr)
+{
+    if (ptr->right != NULL)
+        return bst_minimum(ptr->right);
+    else
+    {
+        struct bst_head* y = ptr->parent;
+        while (y != NULL && ptr == y->right)
+        {
+            ptr = y;
+            y = ptr->parent;
+        }
+        return  y;
+    }
+}
+
+/**
+ * bst predecessor - finds the entry with the largest key smaller than the ptr
+ * @ptr: the current node.
+ */
+struct bst_head* bst_predecessor(struct  bst_head *ptr)
+{
+    if (ptr->left != NULL)
+        return bst_maximum(ptr->left);
+    else
+    {
+        struct bst_head *y = ptr->parent;
+        while (y != NULL && ptr == y->left)
+        {
+            ptr = y;
+            y = ptr->parent;
+        }
+        return  y;
+    }
+}
+
+/**
+ * bst transplant - replace u by v
+ * @u: the subtree u
+ * @v: the subtree v
+ * @root: the tree root
+ */
+void bst_transplant(struct bst_head *u, struct  bst_head *v, struct bst_head *root)
+{
+    if (u->parent == NULL)
+        root = v;
+    else if (u == u->parent->left)
+        u->parent->left = v;
+    else
+        u->parent->right = v;
+
+    if (v != NULL)
+        v->parent = u->parent;
+}
+
+/**
+ * bst_delete - delete ptr in root
+ * @ptr: the entry that want to delete
+ * @root: the tree root
+ */
+void bst_delete(struct bst_head *ptr, struct bst_head *root)
+{
+    if (ptr->left == NULL)
+        bst_transplant(ptr, ptr->right, root);
+    else if (ptr->right == NULL)
+        bst_transplant(ptr, ptr->left, root);
+    else
+    {
+        struct bst_head *y = bst_minimum(ptr->right);
+
+        if (y != NULL)
+        {
+            bst_transplant(y, y->right, root);
+            y->right = ptr->right;
+            y->right->parent = y;
+        }
+
+        bst_transplant(ptr, y, root);
+        y->left = ptr->left;
+        y->left->parent = y;
+    }
+}
+
 
 #endif //BINARY_SEARCH_TREE_H
